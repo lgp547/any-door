f9def85a9881a64ed0fd48dcc8b4aef25733e3e9
lgp <guanpeng.lin@shopcider.com>
2022-11-03 22:26:25 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.5.2"
+}
+
+group = "io.github.lgp547"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
+intellij {
+    version.set("2021.2")
+    type.set("IC") // Target IDE Platform
+
+    plugins.set(listOf(/* Plugin Dependencies */))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("212")
+        untilBuild.set("222.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "any-door-plugin"

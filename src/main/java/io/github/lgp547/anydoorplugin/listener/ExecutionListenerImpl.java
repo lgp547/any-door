8b3c3450165230fd65a51a5e32bf8e44d5f8a073
lgp547 <lgp547279@163.com>
2023-01-09 09:39:14 +0800

    增加尽可能获取可用端口

--- src/main/java/io/github/lgp547/anydoorplugin/listener/ExecutionListenerImpl.java
+++ src/main/java/io/github/lgp547/anydoorplugin/listener/ExecutionListenerImpl.java
@@ -6,6 +6,7 @@ import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.openapi.project.Project;
 import io.github.lgp547.anydoorplugin.settings.AnyDoorSettingsState;
 import io.github.lgp547.anydoorplugin.util.ImportUtil;
+import io.github.lgp547.anydoorplugin.util.PortUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.jetbrains.annotations.NotNull;
@@ -23,12 +24,12 @@ public class ExecutionListenerImpl implements ExecutionListener {
     private void fillAnyDoorPort(ExecutionEnvironment env) {
         if (env.getRunProfile() instanceof ApplicationConfiguration) {
             ApplicationConfiguration runProfile = (ApplicationConfiguration) env.getRunProfile();
-            String str = "-Danydoor.port=";
+            String str = "-DanyDoorOpenPort=";
             if (StringUtils.contains(runProfile.getVMParameters(), str)) {
                 String port = StringUtils.substringBetween(runProfile.getVMParameters(), str, " ");
                 synPort(NumberUtils.toInt(port, -1), env);
             } else {
-                Integer port = getPort();
+                Integer port = getPort(env.getProject().getName());
                 synPort(port, env);
                 runProfile.setVMParameters(runProfile.getVMParameters() + " " + str + port);
             }
@@ -48,10 +49,28 @@ public class ExecutionListenerImpl implements ExecutionListener {
     }
 
     /**
-     * @return available port
+     * @return port
      */
-    private Integer getPort() {
-        // todo:
-        return 6666;
+    private Integer getPort(String projectName) {
+        int num = strToInt(projectName);
+        int port = 1555 + num % 1000;
+        for (int i = 0; i < 10; i++) {
+            if (PortUtil.isPortAvailable(port)) {
+                return port;
+            }
+            port += (int) (Math.random() * 100);
+        }
+        return port;
+    }
+
+    private static int strToInt(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        int sum = 0;
+        for (char c : s.toCharArray()) {
+            sum += c;
+        }
+        return sum;
     }
 }

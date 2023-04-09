e0fc424d734d9945d409cc64a700e2f9c1c61330
lgp <guanpeng.lin@shopcider.com>
2023-04-09 18:41:49 +0800

    增加执行上一次的方法

---
+++ src/main/java/io/github/lgp547/anydoorplugin/action/AnyDoorAgainOpenAction.java
@@ -0,0 +1,32 @@
+package io.github.lgp547.anydoorplugin.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiMethod;
+import io.github.lgp547.anydoorplugin.util.NotifierUtil;
+import org.jetbrains.annotations.NotNull;
+
+public class AnyDoorAgainOpenAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Project project = e.getProject();
+        if (null == project) {
+            throw new IllegalArgumentException("idea arg error (project is null)");
+        }
+        PsiMethod psiMethod = AnyDoorOpenAction.getPreMethod(project.getName());
+        if (null != psiMethod) {
+            try {
+                new AnyDoorPerformed().invoke(project, psiMethod);
+            } catch (Exception exception) {
+                NotifierUtil.notifyError(project, "invoke exception " + exception.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void update(@NotNull final AnActionEvent e) {
+        Project project = e.getProject();
+        e.getPresentation().setEnabledAndVisible(project != null);
+    }
+}
--- src/main/java/io/github/lgp547/anydoorplugin/action/AnyDoorOpenAction.java
+++ src/main/java/io/github/lgp547/anydoorplugin/action/AnyDoorOpenAction.java
@@ -12,14 +12,23 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.containers.FixedHashMap;
 import io.github.lgp547.anydoorplugin.util.NotifierUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Map;
 
-public class AnyDoorAnAction extends AnAction {
+public class AnyDoorOpenAction extends AnAction {
 
     private final static Key<PsiMethod> USER_DATE_ELEMENT_KEY = new Key<>("user.psi.Element");
+
+    private final static Map<String, PsiMethod> PRE_METHOD_MAP = new FixedHashMap<>(10);
+
+    @Nullable
+    public static PsiMethod getPreMethod(String projectName) {
+        return PRE_METHOD_MAP.get(projectName);
+    }
 
     @Override
     public void actionPerformed(@NotNull final AnActionEvent e) {
@@ -42,6 +51,7 @@ public class AnyDoorAnAction extends AnAction {
                 }
             }
             new AnyDoorPerformed().invoke(project, psiMethod);
+            PRE_METHOD_MAP.put(project.getName(), psiMethod);
         } catch (Exception exception) {
             NotifierUtil.notifyError(project, "invoke exception " + exception.getMessage());
         }

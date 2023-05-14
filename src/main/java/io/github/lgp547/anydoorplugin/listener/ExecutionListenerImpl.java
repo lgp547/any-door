c33812d7dac0e883743ad9ddd2176dc82608ee85
lgp547 <lgp547279@163.com>
2023-05-14 18:06:31 +0800

    只对常用的运行进行主动填充pid

--- src/main/java/io/github/lgp547/anydoorplugin/listener/ExecutionListenerImpl.java
+++ src/main/java/io/github/lgp547/anydoorplugin/listener/ExecutionListenerImpl.java
@@ -4,6 +4,7 @@ import com.intellij.execution.ExecutionListener;
 import com.intellij.execution.process.KillableColoredProcessHandler;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.roots.ProjectRootManager;
@@ -11,10 +12,13 @@ import io.github.lgp547.anydoorplugin.settings.AnyDoorSettingsState;
 import io.github.lgp547.anydoorplugin.util.ImportNewUtil;
 import io.github.lgp547.anydoorplugin.util.NotifierUtil;
 import io.github.lgp547.anydoorplugin.util.RuntimeUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class ExecutionListenerImpl implements ExecutionListener {
+
+    private static final Logger log = Logger.getInstance(ExecutionListenerImpl.class);
 
     @Override
     public void processStartScheduled(@NotNull String executorId, @NotNull ExecutionEnvironment env) {
@@ -29,10 +33,16 @@ public class ExecutionListenerImpl implements ExecutionListener {
     private void fillRunProjectPid(@NotNull ExecutionEnvironment env, @NotNull ProcessHandler handler) {
         Project project = env.getProject();
         try {
+            String runClassName = env.getRunProfile().getClass().getName();
+            if (!StringUtils.endsWith(runClassName, "SpringBootApplicationRunConfiguration") && !StringUtils.endsWithIgnoreCase(runClassName, "ApplicationConfiguration")) {
+                log.debug("fillRunProjectPid runClassName is not match {}", runClassName);
+                return;
+            }
             AnyDoorSettingsState anyDoorSettingsState = AnyDoorSettingsState.getAnyDoorSettingsState(project);
             if (handler instanceof KillableColoredProcessHandler.Silent) {
                 anyDoorSettingsState.pid = ((KillableColoredProcessHandler.Silent) handler).getProcess().pid();
             } else {
+                log.debug("fillRunProjectPid !handler instanceof KillableColoredProcessHandler.Silent");
                 // jps
                 String basePath = "";
                 Sdk projectSdk = ProjectRootManager.getInstance(project).getProjectSdk();

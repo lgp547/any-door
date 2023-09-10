91cd83a2a8aa7d3b64d8c3bc57588d3250b98a86
lgp <guanpeng.lin@shopcider.com>
2023-09-10 18:57:09 +0800

    修复懒加载情况下会有无法实例化类情况

--- any-door-common/src/main/java/io/github/lgp547/anydoor/common/util/AnyDoorSpringUtil.java
+++ any-door-common/src/main/java/io/github/lgp547/anydoor/common/util/AnyDoorSpringUtil.java
@@ -3,6 +3,7 @@ package io.github.lgp547.anydoor.common.util;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
@@ -28,23 +29,36 @@ public class AnyDoorSpringUtil {
             synchronized (AnyDoorSpringUtil.class) {
                 if (!isInit) {
                     AnyDoorSpringUtil.applicationContextsSupplier = applicationContextsSupplier;
-                    doInit();
+                    updateApplicationContextList();
+                    setClassLoader();
                     isInit = true;
                 }
             }
         }
     }
 
-    private static void doInit() {
-        List<ApplicationContext> curApplicationContextList = updateApplicationContextList();
+    /**
+     * 由于没有set进去，会从执行线程获取类加载器
+     * 当处于懒加载期间，会有获取到 AnyDoorClassloader 的情况，这里兼容处理
+     */
+    private static void setClassLoader() {
+        for (ApplicationContext applicationContext : applicationContextList) {
+            if (applicationContext instanceof DefaultResourceLoader) {
+                ((DefaultResourceLoader) applicationContext).setClassLoader(applicationContext.getClassLoader());
+            }
+        }
+    }
 
+    private static List<ApplicationContext> sortList(List<ApplicationContext> curApplicationContextList) {
         if (curApplicationContextList.size() > 1) {
             String contextName = System.getProperty("anydoor.spring.core.context.name");
             if (StringUtils.hasLength(contextName)) {
                 Optional<ApplicationContext> any = curApplicationContextList.stream().filter(item -> item.getDisplayName().contains(contextName)).findAny();
                 if (any.isPresent()) {
-                    AnyDoorSpringUtil.applicationContextList = new ArrayList<>();
-                    AnyDoorSpringUtil.applicationContextList.add(any.get());
+                    curApplicationContextList.clear();
+                    curApplicationContextList.add(any.get());
+                } else {
+                    throw new IllegalArgumentException("not found context name: " + contextName);
                 }
             } else {
                 ArrayList<String> contextNames = new ArrayList<>();
@@ -52,9 +66,10 @@ public class AnyDoorSpringUtil {
                 contextNames.add("ReactiveWebApplicationContext");
                 contextNames.add("WebApplicationContext");
                 contextNames.add("ConfigurableApplicationContext");
-                AnyDoorSpringUtil.applicationContextList.sort(Comparator.comparing(ApplicationContext::getDisplayName, (name1, name2) -> comparingSort(contextNames, name1, name2)));
+                curApplicationContextList.sort(Comparator.comparing(ApplicationContext::getDisplayName, (name1, name2) -> comparingSort(contextNames, name1, name2)));
             }
         }
+        return curApplicationContextList;
     }
 
     private static int comparingSort(List<String> sort, String name1, String name2) {
@@ -70,22 +85,26 @@ public class AnyDoorSpringUtil {
     }
 
     private static List<ApplicationContext> updateApplicationContextList() {
-        ApplicationContext[] array = applicationContextsSupplier.get();
-        if (array == null || array.length == 0) {
-            AnyDoorSpringUtil.applicationContextList = Collections.emptyList();
-        } else {
-            AnyDoorSpringUtil.applicationContextList = Arrays.stream(array).collect(Collectors.toList());
+        synchronized (AnyDoorSpringUtil.class) {
+            ApplicationContext[] array = applicationContextsSupplier.get();
+            if (array == null || array.length == 0) {
+                AnyDoorSpringUtil.applicationContextList = Collections.emptyList();
+            } else {
+                AnyDoorSpringUtil.applicationContextList = Arrays.stream(array).collect(Collectors.toList());
+            }
+            return sortList(AnyDoorSpringUtil.applicationContextList);
         }
-        return AnyDoorSpringUtil.applicationContextList;
     }
 
     public static <T> T getBean(Class<T> requiredType) throws BeansException {
-        Objects.requireNonNull(applicationContextList);
+        synchronized (AnyDoorSpringUtil.class) {
+            Objects.requireNonNull(applicationContextList);
 
-        try {
-            return doGetBean(requiredType, applicationContextList);
-        } catch (IllegalStateException illegalStateException) {
-            return doGetBean(requiredType, updateApplicationContextList());
+            try {
+                return doGetBean(requiredType, applicationContextList);
+            } catch (Exception exception) {
+                return doGetBean(requiredType, updateApplicationContextList());
+            }
         }
     }
 

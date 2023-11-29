8073f61c4434c0447c5e1e4b168b390133d2044f
lgp <lgp547279@163.com>
2023-11-29 11:24:41 +0800

    升级版本2.1.0

--- any-door-plugin/build.gradle.kts
+++ any-door-plugin/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "io.github.lgp547"
-version = "2.0.2"
+version = "2.1.0"
 
 repositories {
     mavenCentral()
@@ -14,8 +14,8 @@ repositories {
 }
 
 dependencies {
-    implementation("io.github.lgp547:any-door-core:2.0.2")
-    implementation("io.github.lgp547:any-door-attach:2.0.2")
+    implementation("io.github.lgp547:any-door-core:2.1.0")
+    implementation("io.github.lgp547:any-door-attach:2.1.0")
 
 }
 
